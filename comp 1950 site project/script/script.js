fa5136f3e26b28f2f2ade15867835eb730651c8c
Xantosuex <xantosuex@hotmail.com>
2014-03-29 21:21:31 -0700

    WORKING JQUERY

--- comp 1950 site project/script/script.js
+++ comp 1950 site project/script/script.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-	var content = 60;
-	$(".first").click(function() {
-		if(content < 1) {
-			$(right-content).animate({left:'-60%'});
-			content = 60;
-		} else {
-			$(right-content).animate({left:'0%'});
-			content = 0;
-		}
-	});
+
+$(".first").click(function(){
+
+$("#right-content").animate(
+		{left: '0%'}
+	);
+
 });
+
+
+
+
+
+
+});
+
+
