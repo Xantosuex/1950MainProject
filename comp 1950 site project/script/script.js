d8f0a25821178932a7dda71f3c0bca45dab5f9ef
Xantosuex <xantosuex@hotmail.com>
2014-03-30 01:47:18 -0700

    Even Better JQuery

--- comp 1950 site project/script/script.js
+++ comp 1950 site project/script/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
-
-$(".first").click(function(){
-
-$("#right-content").animate(
-		{left: '0%'}
-	);
-
+	var content = 60;
+	$(".first").click(function() {
+		if(content < 1) {
+			$("#right-content").animate({left:'-60%'});
+			content = 60;
+		} else {
+			$("#right-content").animate({left:'0%'});
+			content = 0;
+		}
+	});
 });
-
-
-
-
-
-
-});
-
-
