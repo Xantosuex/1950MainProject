6a352133e238ed427be89eb231734090bb3f343b
Xantosuex <xantosuex@hotmail.com>
2014-03-29 20:53:15 -0700

    Working jQuery

---
+++ comp 1950 site project/script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var content = 60;
+	$(".first").click(function() {
+		if(content < 1) {
+			$(right-content).animate({left:'-60%'});
+			content = 60;
+		} else {
+			$(right-content).animate({left:'0%'});
+			content = 0;
+		}
+	});
+});
